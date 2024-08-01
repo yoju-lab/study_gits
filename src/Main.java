cbb4d6b37e28b730b9ed0523277f422074776004
jjang051jhta <jjang051jhta@gmail.com>
2024-08-01 15:20:40 +0900

    임시 저장

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<HashMap<String, String>> questions = new ArrayList<>();
+
+        // 첫 번째 문제
+        HashMap<String, String> question1 = new HashMap<>();
+        question1.put("question", "1. Python에서 변수를 선언하는 방법은? (점수: 10점)");
+        question1.put("choices", "1) var name 2) name = value 3) set name 4) name == value");
+        question1.put("correct", "2");
+        question1.put("score", "10");
+        questions.add(question1);
         System.out.println("Hello world!");
     }
 }
